862c8f8cbccde05581f4518e09eb596df9abee7d
DinuGaro <dineinger.garo@gmail.com>
2023-07-10 02:42:45 +0200

    remove obsolete code

--- OnStackGenerator/OnStackGenerator.cs
+++ OnStackGenerator/OnStackGenerator.cs
@@ -23,10 +23,6 @@ public class OnStackGenerator : IIncrementalGenerator
             {
                 var classesWithAttribute = GetClassesWithAttribute(compilation);
 
-                StringBuilder sb = new StringBuilder();
-                sb.AppendLine("/*");
-                //CreateHeader(sb, "OnStackTests");
-
                 foreach (var a in classesWithAttribute)
                 {
                     var fields = GetFields(a);
@@ -44,21 +40,8 @@ public class OnStackGenerator : IIncrementalGenerator
                     var fileText = OnStackStructCreator.CreateClassForFile(onStackTypeInfo)
                         .ToString();
 
-                    sb.AppendLine(typeId);
-
                     context.AddSource($"{onStackTypeInfo.TypeName}.g.cs", fileText);
-
-                    //CreateClassAndStruct(sb, typeId);
-
-                    //CreateFields(sb, fields);
-
-                    //CreateAllocateMethod(sb, typeId, fields);
-
-                    //CloseClassAndStruct(sb);
                 }
-
-                sb.Append("*/");
-                context.AddSource("OnStack.g.cs", sb.ToString());
             });
 
     }
@@ -79,50 +62,6 @@ public class OnStackGenerator : IIncrementalGenerator
         }
 
         return result;
-    }
-
-    private static void CreateAllocateMethod(StringBuilder sb, string typeId, IEnumerable<FieldDeclarationSyntax> fields)
-    {
-        sb.Append("        public ");
-        sb.Append(typeId);
-        sb.AppendLine(" Allocate()");
-        sb.AppendLine("        {");
-        sb.Append("            return new ");
-        sb.Append(typeId);
-        sb.AppendLine("()");
-        sb.AppendLine("            {");
-        foreach (var field in fields)
-        {
-            var name = GetFieldName(field);
-
-            sb.Append("                ");
-            sb.Append(name);
-            sb.Append(" = this.");
-            sb.Append(name);
-            sb.AppendLine(",");
-        }
-        sb.AppendLine("            };");
-        sb.AppendLine("        }");
-    }
-
-    private static void CreateFields(StringBuilder sb, IEnumerable<FieldDeclarationSyntax> fields)
-    {
-        foreach (var field in fields)
-        {
-            var type = GetFieldType(field);
-            var name = GetFieldName(field);
-
-            sb.Append("        public ");
-            sb.Append(type.Type);
-            if (type.IsNullable)
-            {
-                sb.Append('?');
-            }
-            sb.Append(" ");
-            sb.Append(name);
-            sb.AppendLine(";");
-            sb.AppendLine();
-        }
     }
 
     private static (bool IsNullable, string Type) GetFieldType(FieldDeclarationSyntax field)
@@ -176,31 +115,6 @@ public class OnStackGenerator : IIncrementalGenerator
                     {
                         return a.Name.GetText().ToString() == "OnStack";
                     })));
-    }
-
-    private static void CreateHeader(StringBuilder sb, string @namespace)
-    {
-        sb.AppendLine("// Auto-generated code");
-        sb.AppendLine();
-        sb.Append("namespace ");
-        sb.Append(@namespace);
-        sb.Append(";");
-        sb.AppendLine();
-        sb.AppendLine("#nullable enable");
-    }
-
-    private static void CreateClassAndStruct(StringBuilder sb, string typeId)
-    {
-        sb.Append("public partial class ").AppendLine(typeId);
-        sb.AppendLine("{");
-        sb.AppendLine("    public struct OnStack");
-        sb.AppendLine("    {");
-    }
-
-    private static void CloseClassAndStruct(StringBuilder sb)
-    {
-        sb.AppendLine("    }");
-        sb.AppendLine("}");
     }
 }
 
